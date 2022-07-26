17143e7772d0e55d012efb7096426b9c8ce4e388
woosukji <woosukji@snu.ac.kr>
2022-07-26 14:26:27 +0900

    refactor out depth capturing feature

---
+++ voyager/DepthSaver.swift
@@ -0,0 +1,77 @@
+//
+//  DepthSaver.swift
+//  voyager
+//
+//  Created by 지우석 on 2022/07/26.
+//
+
+import UIKit
+import ARKit
+
+class DepthSaver {
+    
+    weak var arSession: ARSession!
+    
+    init(session: ARSession) {
+        self.arSession = session
+    }
+    
+    func saveDepthToCilpBoard() throws {
+        
+        guard let currentDepthMap = arSession.currentFrame?.smoothedSceneDepth?.depthMap else {
+            throw NSError()
+        }
+        
+        let depthAsArray = get2DimArrayFromDepthMap(depth: currentDepthMap)
+        UIPasteboard.general.string = getStringFrom2DimArray(array: depthAsArray,
+                                                             height: CVPixelBufferGetHeight(currentDepthMap),
+                                                             width: CVPixelBufferGetWidth(currentDepthMap))
+    }
+    
+    func get2DimArrayFromDepthMap(depth: CVPixelBuffer) -> [[Float32]] {
+        let depthWidth = CVPixelBufferGetWidth(depth)
+        let depthHeight = CVPixelBufferGetHeight(depth)
+        
+        CVPixelBufferLockBaseAddress(depth, CVPixelBufferLockFlags(rawValue: 0))
+        
+        let floatBuffer = unsafeBitCast(CVPixelBufferGetBaseAddress(depth),to: UnsafeMutablePointer<Float32>.self)
+        
+        var depthArray = [[Float32]]()
+        
+        for y in 0...depthHeight-1 {
+            var distancesLine = [Float32]()
+            for x in 0...depthWidth-1 {
+                let distanceAtXYPoint = floatBuffer[y * depthWidth + x]
+                distancesLine.append(distanceAtXYPoint)
+//                print("Depth in (\(x),\(y)): \(distanceAtXYPoint)")
+            }
+            depthArray.append(distancesLine)
+        }
+        
+        return depthArray
+    }
+    // Read more at https://www.it-jim.com/blog/iphones-12-pro-lidar-how-to-get-and-interpret-data/
+    
+    
+    // Auxiliary function to make String from depth map array
+    func getStringFrom2DimArray(array: [[Float32]], height: Int, width: Int) -> String{
+        var arrayStr: String = ""
+        
+        for y in 1...height-1 {
+            var lineStr = ""
+            
+            for x in 1...width-1 {
+                
+                lineStr += String(array[y][x])
+                if x != width-1 {
+                    lineStr += ","
+                }
+            }
+            lineStr += "\n"
+            arrayStr += lineStr
+        }
+        return arrayStr
+    }
+    // Read more at https://www.it-jim.com/blog/iphones-12-pro-lidar-how-to-get-and-interpret-data/
+    
+}
--- voyager/ViewController.swift
+++ voyager/ViewController.swift
@@ -10,12 +10,15 @@ import SceneKit
 import ARKit
 import Toast_Swift
 
-class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
+class ViewController: UIViewController, ARSCNViewDelegate {
     
     @IBOutlet weak var sceneView: ARSCNView!
     @IBOutlet weak var imgView: UIImageView!
     
     var currentDepthMap: CVPixelBuffer?
+    
+    var arSession: ARSession!
+    var depthSaver: DepthSaver!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,65 +26,10 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         sceneView.session.delegate = self
+        
+        self.arSession = sceneView.session
+        self.depthSaver = DepthSaver(session: self.arSession)
     }
-    
-    @IBAction func handleCaptureButton(_ sender: Any) {
-        guard let currentDepthMap = currentDepthMap else {
-            view.makeToast("Unable to retrieve LiDAR Sensor data!")
-            return
-        }
-
-        let depthAsArray = get2DimArrayFromDepthMap(depth: currentDepthMap)
-        UIPasteboard.general.string = getStringFrom2DimArray(array: depthAsArray,
-                                                             height: CVPixelBufferGetHeight(currentDepthMap),
-                                                             width: CVPixelBufferGetWidth(currentDepthMap))
-        view.makeToast("LiDAR Sensor data copied to clipboard!")
-    }
-    
-    func get2DimArrayFromDepthMap(depth: CVPixelBuffer) -> [[Float32]] {
-        let depthWidth = CVPixelBufferGetWidth(depth)
-        let depthHeight = CVPixelBufferGetHeight(depth)
-        
-        CVPixelBufferLockBaseAddress(depth, CVPixelBufferLockFlags(rawValue: 0))
-        
-        let floatBuffer = unsafeBitCast(CVPixelBufferGetBaseAddress(depth),to: UnsafeMutablePointer<Float32>.self)
-        
-        var depthArray = [[Float32]]()
-        
-        for y in 0...depthHeight-1 {
-            var distancesLine = [Float32]()
-            for x in 0...depthWidth-1 {
-                let distanceAtXYPoint = floatBuffer[y * depthWidth + x]
-                distancesLine.append(distanceAtXYPoint)
-                print("Depth in (\(x),\(y)): \(distanceAtXYPoint)")
-            }
-            depthArray.append(distancesLine)
-        }
-        
-        return depthArray
-    }
-    // Read more at https://www.it-jim.com/blog/iphones-12-pro-lidar-how-to-get-and-interpret-data/
-    
-    // Auxiliary function to make String from depth map array
-    func getStringFrom2DimArray(array: [[Float32]], height: Int, width: Int) -> String{
-        var arrayStr: String = ""
-        
-        for y in 1...height-1 {
-            var lineStr = ""
-            
-            for x in 1...width-1 {
-                
-                lineStr += String(array[y][x])
-                if x != width-1 {
-                    lineStr += ","
-                }
-            }
-            lineStr += "\n"
-            arrayStr += lineStr
-        }
-        return arrayStr
-    }
-    // Read more at https://www.it-jim.com/blog/iphones-12-pro-lidar-how-to-get-and-interpret-data/
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -95,22 +43,30 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
 
         configuration.frameSemantics = [.smoothedSceneDepth]
         
-        /// adjust frames for sceneview
-//        sceneView.preferredFramesPerSecond = 15
-        
         // Run the view's session
         sceneView.session.run(configuration)
+        
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
-        /// access to ARFrame obj for current timestamp
-//        sceneView.session.currentFrame
-        
         // Pause the view's session
         sceneView.session.pause()
     }
+    
+    @IBAction func handleCaptureButton(_ sender: Any) {
+        do {
+            try depthSaver.saveDepthToCilpBoard()
+            self.view.makeToast("LiDAR Sensor data copied to clipboard!")
+        } catch {
+            self.view.makeToast("Unable to retrieve LiDAR Sensor data!")
+        }
+    }
+    
+}
+
+extension ViewController: ARSessionDelegate {
     
     func session(_ session: ARSession, didUpdate frame: ARFrame) {
         
