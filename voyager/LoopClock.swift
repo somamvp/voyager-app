25837a7c3024b4fb1a59a076f88eee1730002fbd
woosukji <woosukji@snu.ac.kr>
2022-07-26 16:03:43 +0900

    MVP-79 [feat] add clock object for looping #close
    - `LoopClock' 이 timer loop의 관리와 호출을 담당
    - 정해진 시간간격마다 (기본값 0.1초), `LoopClockDelegate` 의 delegate 메서드 호출

---
+++ voyager/LoopClock.swift
@@ -0,0 +1,51 @@
+//
+//  LoopClock.swift
+//  voyager
+//
+//  Created by 지우석 on 2022/07/26.
+//
+
+import Foundation
+
+protocol LoopClockDelegate: AnyObject {
+    func invoke()
+}
+
+class LoopClock {
+    
+    var timer: Timer?
+    weak var delegate: LoopClockDelegate?
+    
+    let fps: Int
+    let timeIntervalInSeconds: Double
+    
+    private(set) var counter = 0
+    
+    init(fps: Int) {
+        self.fps = fps
+        self.timeIntervalInSeconds = 1.0 / Double(fps)
+    }
+    
+    func start() {
+        print("starting timer")
+        
+        if let previousTimer = timer {
+            print("trying to start timer when there is existing one. invalidating previous timer.")
+            previousTimer.invalidate()
+        }
+        
+        self.counter = 0
+        
+        self.timer = Timer.scheduledTimer(withTimeInterval: timeIntervalInSeconds, repeats: true) { [weak self] timer in
+            self?.delegate?.invoke()
+            self?.counter += 1
+        }
+    }
+    
+    func stop() {
+        print("stopping timer")
+        
+        self.timer?.invalidate()
+        self.timer = nil
+    }
+}
