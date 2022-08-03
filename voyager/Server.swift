f1295f702891dd06d004517bb7ba62d5ca5d6f98
woosukji <woosukji@snu.ac.kr>
2022-08-03 11:48:10 +0900

    MVP-82 [feat] add image resizing feature & receive YOLO result data

--- voyager/Server.swift
+++ voyager/Server.swift
@@ -13,20 +13,41 @@ import UIKit
 
 struct ServerImageUploadData {
     static var compressionQuality = 0.5
+    static var targetSize = CGSize(width: 160, height: 320)
     
     var filename = "test.jpg"
     var sequenceNo = 0
     var image: CVPixelBuffer
     var imageData: Data? {
-        let ciImage = CIImage(cvPixelBuffer: image)
-        let uiImage = UIImage(ciImage: ciImage)
+        let ciImage = CIImage(cvPixelBuffer: image).oriented(.right)
+        let scaledUIImage = UIImage(ciImage: ciImage).scalePreservingAspectRatio(targetSize: ServerImageUploadData.targetSize)
         
-        return uiImage.jpegData(compressionQuality: ServerImageUploadData.compressionQuality)
+        return scaledUIImage.jpegData(compressionQuality: ServerImageUploadData.compressionQuality)
     }
 }
 
 struct ServerImageResponseRawData: Decodable {
     let sequenceNo: Int
+}
+
+struct ServerYoloResponseRawData: Decodable {
+    let xmin: Double
+    let xmax: Double
+    let ymin: Double
+    let ymax: Double
+    
+    let classId: Int
+    let className: String
+    
+    enum CodingKeys: String, CodingKey {
+        case xmin
+        case xmax
+        case ymin
+        case ymax
+        
+        case classId = "class"
+        case className = "name"
+    }
 }
 
 class Server {
@@ -58,8 +79,11 @@ class Server {
             
             multipartFormData.append(imgData.imageData!, withName: "img", fileName: imgData.filename, mimeType: "image/jpg")
                 
-        }, to: serverImageUploadURI!).responseDecodable(of: ServerImageResponseRawData.self) { response in
+        }, to: serverImageUploadURI!).responseDecodable(of: [ServerYoloResponseRawData].self) { response in
             print("response recieved: \(response)")
+            if case .failure = response.result {
+                print(response.debugDescription)
+            }
         }
         
     }
