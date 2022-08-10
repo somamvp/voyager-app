291fd9b9b820666ccff77299f455b3575742f86b
woosukji <woosukji@snu.ac.kr>
2022-08-10 21:07:59 +0900

    MVP-100 [fix] fix opencv compile problem #close

--- voyager/ImageProcessing/OpenCV.h
+++ voyager/ImageProcessing/OpenCV.h
@@ -7,18 +7,13 @@
 
 #import <Foundation/Foundation.h>
 #import "OpenCV.h"
-#import <opencv2/imgcodecs/ios.h>
-#import <opencv2/opencv.hpp>
+#import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface OpenCV : NSObject
 
 + (UIImage *)toGray:(UIImage *)image;
-
-+ (double)max:(cv::Mat)M;
-
-+ (double)min:(cv::Mat)M;
 
 @end
 
--- voyager/ImageProcessing/OpenCV.mm
+++ voyager/ImageProcessing/OpenCV.mm
@@ -6,6 +6,8 @@
 //
 
 #import "OpenCV.h"
+#import <opencv2/imgcodecs/ios.h>
+#import <opencv2/opencv.hpp>
 
 @implementation OpenCV
 
@@ -22,7 +24,7 @@
     return max;
 }
 
-+ (double)min:(cv::Mat)M {
++ (double) min: (cv::Mat)M {
     // compute the sum of positive matrix elements, optimized variant
     double min=0;
     int cols = M.cols, rows = M.rows;
@@ -39,20 +41,19 @@
     //Transform UIImage to cv::Mat
     cv::Mat imageMat;
     UIImageToMat(image, imageMat);
-    
+
     //If the image was alreay grayscale, return it
     if (imageMat.channels() == 1)
         return image;
-    
+
     NSLog(@"image size: %@", imageMat.size);
-    NSLog(@"image min: %f", min(imageMat));
-    NSLog(@"image max: %f", max(imageMat));
-    
-    
+    NSLog(@"image min: %f", [OpenCV min:imageMat]);
+    NSLog(@"image max: %f", [OpenCV max:imageMat]);
+ 
     //Transform the cv::mat color image to gray
     cv::Mat grayMat;
     cv::cvtColor(imageMat, grayMat, cv::COLOR_BGR2GRAY);
-    
+
     //Transform grayMat to UIImage and return
     return MatToUIImage(grayMat);
 }
